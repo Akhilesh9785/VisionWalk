b944119c4a70a9319978b39307b79a9249d94f5e
Akhilesh9785 <167116655+Akhilesh9785@users.noreply.github.com>
2024-04-17 15:15:54 +0530

    new cc

---
+++ Assets/Scripts/SetNavigationTarget.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class setNavigationTarget : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+
+    private Camera TopDownCamera;
+    [SerializeField]
+    private GameObject navTargetObject;
+    private NavMeshPath path;
+    private LineRenderer line;
+    private bool lineToggle = false;
+
+    private void start(){
+        path = new NavMeshPath();
+        line = transform.GetComponent<LineRenderer>();
+
+    }
+
+    private void update(){
+        if((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began )){
+            lineToggle = !lineToggle;
+        }
+
+    if(lineToggle){
+        NavMesh.CalculatePath(transform.position, navTargetObject.transform.position, NavMesh.AllAreas, path);
+        line.positionCount = path.corners.Length;
+        line.SetPositions(path.corners);
+        line.enabled = true;
+    }
+
+    }
+   
+}
